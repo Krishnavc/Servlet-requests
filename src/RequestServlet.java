1f45d6c39fe36e12ee61d3e70384bc439efd926f
Krishnaveni <Krishnaveni@192.168.0.7>
2017-01-17 11:01:01 -0600

    Servlet Request

---
+++ src/RequestServlet.java
@@ -0,0 +1,61 @@
+
+import javax.servlet.http.*;  
+import javax.servlet.*;  
+import java.io.*; 
+
+public class RequestServlet extends HttpServlet {  
+public void doGet(HttpServletRequest req,HttpServletResponse res)  
+throws ServletException,IOException  
+{  
+res.setContentType("text/html");  
+PrintWriter pw=res.getWriter();  
+  
+String name=req.getParameter("name");//will return value  
+pw.println("Request "+name);  
+  
+pw.close();  
+
+}
+}
+/*
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestServlet
+ */
+/*
+@WebServlet("/RequestServlet")
+public class RequestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+ /*   public RequestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+/*	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+/*	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+*/
